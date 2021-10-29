31c6699f74b3c0a8f06c9e741fdb90ceab07978b
Sandyabhi <abhisandy47@gmail.com>
2021-10-29 23:42:40 +0530

    apicall

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const authRoute = require("./routes/auth");
+
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+//middleware
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+
+
+app.listen(8000, () => {
+  console.log("Backend server is running");
+});
